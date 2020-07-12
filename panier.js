cbdb8d5797a0840d964147f7c73384b35f68680f
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2020-07-12 12:09:35 +0200

    Update panier.js

--- panier.js
+++ panier.js
@@ -1,20 +1,9 @@
 let products = JSON.parse(localStorage.getItem('productsInCart'));
 
 for(let product of products){
-	let img = document.getElementsByClassName("cart-item-image");
-	let titre = document.getElementsByClassName("cart-item-tile");
-	let prix = document.getElementById("prix");
-
+	let img = document.querySelector(".cart-item-image");
+	document.querySelector(".cart-item-title").innerText = product.name;
+	document.querySelector(".price").innerText = product.price;
 
 	img.src = product.image;
-	titre.textContent = product.name;
-	prix = product.price;
-
 }
-
-
-
-
-
-
-
