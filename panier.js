8106838267db83c6e3a7ef288bd385eb52c6db1b
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2020-07-14 09:06:33 +0200

    Update panier.js

--- panier.js
+++ panier.js
@@ -1,3 +1,12 @@
+let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+for (let i = 0; i < removeCartItemButtons.length;  i++){
+	let button = removeCartItemButtons[i]
+	button.addEventListener('click', function(event){
+		let buttonClicked = event.target
+		buttonClicked.parentElement.parentElement.remove()	
+	})
+}
+
 let products = JSON.parse(localStorage.getItem('productsInCart'));
 
 for(let product of products){
