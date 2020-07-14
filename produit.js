1d1b5203aaced328d0f3542507e1f6ca68ceb1bb
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2020-07-14 10:31:30 +0200

    Update produit.js

--- produit.js
+++ produit.js
@@ -1,60 +1,18 @@
-let params = new URLSearchParams(window.location.search);
-let idTeddy = params.get("id");
-let url = "http://localhost:3000/api/teddies/" + idTeddy;
-
-fetch(url)
-	.then((response) =>
-		response.json().then((teddy) => {
-
-			let container = document.getElementById("container");
-			let produit = document.createElement("div");
-			let image = document.createElement("img");
-			let nom = document.createElement("h2");
-			let prix = document.createElement("h3");
-			let description = document.createElement("div");
-			let select = document.createElement("select");
-			let option = document.createElement("option");
-			let btn = document.createElement("a");
-			
-			produit.setAttribute("class", "produit");
-			btn.setAttribute("class", "btn");
-
-			container.appendChild(produit);
-			produit.appendChild(image);
-			produit.appendChild(nom);
-			produit.appendChild(prix);
-			produit.appendChild(description);
-			produit.appendChild(select);
-			for (let color of teddy.colors){
-				let option = document.createElement("option");
-				select.appendChild(option);
-				option.textContent = color;
-			}
-			produit.appendChild(btn);
-
-			image.src = teddy.imageUrl;
-			nom.textContent = teddy.name;
-			prix.textContent = teddy.price += " €";
-			description.textContent = teddy.description;
-			btn.textContent = "Ajouter";
-			btn.href = "panier.html";
-			
-			let infoProduit = {
-				name : teddy.name,
-				price : teddy.price,
-				image : teddy.imageUrl,
-			}
-			
-			if(localStorage.getItem("productsInCart") === null) {
-				let productsInCart = [];
-				localStorage.setItem("productsInCart", JSON.stringify(productsInCart));
-			}
-			let productsInCart = JSON.parse(localStorage.getItem("productsInCart"));
-
-			productsInCart.push(infoProduit);
-			btn.addEventListener("click", () => {
-			localStorage.setItem("productsInCart", JSON.stringify(productsInCart));
-		})
+let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+for (let i = 0; i < removeCartItemButtons.length;  i++){
+	let button = removeCartItemButtons[i]
+	button.addEventListener('click', function(event){
+		let buttonClicked = event.target
+		buttonClicked.parentElement.parentElement.remove()	
 	})
-)
-	.catch((err) => console.log("Erreur : " + err));
+}
+
+let products = JSON.parse(localStorage.getItem('productsInCart'));
+
+for (let product of products){
+	let img = document.querySelector(".cart-item-image");
+	document.querySelector(".cart-item-title").innerText = product.name;
+	document.querySelector(".price").innerText = product.price;
+	img.src = product.image;
+	console.log(product)
+}
