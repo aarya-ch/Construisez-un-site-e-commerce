e37f9b4afe2561188c3257e58390840e6f21edcd
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2020-08-17 11:07:05 +0200

    Update confirmation.js

--- confirmation.js
+++ confirmation.js
@@ -3,6 +3,9 @@ let total = params.get("total");
 let totale = document.getElementById("prixTotal");
 totale.textContent = total + " €";
 
+let order = params.get("order");
+document.getElementById("order").textContent = order;
+
 let contact;
 if(localStorage.getItem("contact") !== null){
 	contact = JSON.parse(localStorage.getItem("contact"));
