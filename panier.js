d43bc812fcb31b7c46af9cd536f616e966637538
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2020-08-18 10:38:05 +0200

    Update panier.js

--- panier.js
+++ panier.js
@@ -55,8 +55,9 @@ function total_price(products){
 	for (let i = 0; i < products.length; i++) {
 		total += products[i].price;
 	}
-	return total;
+	return total + " €";
 }
+
 let products = JSON.parse(localStorage.getItem('productsInCart'));
 
 if (products) {
@@ -67,78 +68,77 @@ if (products) {
 
 		let cartRow = create_cart_row();
 		let item = create_item(); 
-		let itemImage = create_image(product.image);
-		let itemTitle = create_title(product.name);
-		let cartPrice = create_price(product.price);
 		
 		items.appendChild(cartRow);
 		cartRow.appendChild(item);
-		item.appendChild(itemImage);
-		item.appendChild(itemTitle);
-		cartRow.appendChild(cartPrice);
+		item.appendChild(create_image(product.image));
+		item.appendChild(create_title(product.name));
+		cartRow.appendChild(create_price(product.price));
 
 		product.price = parseInt(product.price);
 	}
-//Création d'un bouton pour supprimer les éléments du bouton
-let remove_btn = document.getElementById('btn-remove');
-	remove_btn.addEventListener('click', function(event){
+
+	let totale = document.querySelector(".cart-total-price");
+
+	//Création d'un bouton pour supprimer les éléments du bouton
+	let remove_btn = document.getElementById('btn-remove');
+	remove_btn.addEventListener('click', function(){
 		document.getElementById("container").remove();
 		localStorage.clear();
-		let emptyCart = document.querySelector(".cart-total-price");
-		emptyCart.textContent = 0 + ' €';
-	})
-
-let totale = document.querySelector(".cart-total-price");
-totale.textContent = total_price(products) + ' €';
-
-// Formulaire //
-let contact = {};
-
-function checkField(regex, id){
-	let elemValue = document.getElementById(id).value;
-	if(regex.test(elemValue)){
-		document.getElementById('erreur' + id).innerHTML = "";
-		contact[id] = elemValue;
-		return 1;
-	} else {
-		document.getElementById('erreur' + id).innerHTML = "* " + id + " invalide ";
-		contact[id] = "";
-		return 0;
-	}
-}
-
-document.getElementById("btn-purchase").addEventListener("click", function(){
-
-	let checkedElements = 0;
-
-	checkedElements += checkField(new RegExp(/^[A-Za-z]{2,30}$/), "firstName");
-	checkedElements += checkField(new RegExp(/^[A-Za-z]{2,30}$/), "lastName");
-	checkedElements += checkField(new RegExp(/^[A-Za-z0-9]/), "address");
-	checkedElements += checkField(new RegExp(/^[A-Za-z-]{1,38}$/), "city");
-	checkedElements += checkField(new RegExp(/^[A-Za-z-_.0-9]{3,}@[a-z]{3,}[.]{1}[a-z]{2,3}$/), "email");
-
-	let panier = {
-		contact: contact, 
-		products: generate_products_id(products)
-	}
-
-	const options = {
-		method: 'POST',
-		body: JSON.stringify(panier),
-		headers: {
-			'Content-Type': 'application/json'
-		}	
-	}
-
-	console.log(panier);
-
-	fetch('http://localhost:3000/api/teddies/order', options)
-	.then(res => res.json())
-	.then(res => {
-		window.location.replace("file:///C:/Users/Arujan/Desktop/JWDP5-master/confirmation.html?total=" + total + "&order=" + res.orderId);
-	})
-	.catch((error) => {
-		console.error('Error:', error);
+		totale.textContent = 0 + ' €';
 	});
-});
+
+
+	totale.textContent = total_price(products);
+
+	// Formulaire //
+	let contact = {};
+
+	function checkField(regex, id){
+		let elemValue = document.getElementById(id).value;
+		if(regex.test(elemValue)){
+			document.getElementById('erreur' + id).innerHTML = "";
+			contact[id] = elemValue;
+			return 1;
+		} else {
+			document.getElementById('erreur' + id).innerHTML = "* " + id + " invalide ";
+			contact[id] = "";
+			return 0;
+		}
+	}
+
+	document.getElementById("btn-purchase").addEventListener("click", function(){
+
+		let checkedElements = 0;
+
+		checkedElements += checkField(new RegExp(/^[A-Za-z]{2,30}$/), "firstName");
+		checkedElements += checkField(new RegExp(/^[A-Za-z]{2,30}$/), "lastName");
+		checkedElements += checkField(new RegExp(/^[A-Za-z0-9]/), "address");
+		checkedElements += checkField(new RegExp(/^[A-Za-z-]{1,38}$/), "city");
+		checkedElements += checkField(new RegExp(/^[A-Za-z-_.0-9]{3,}@[a-z]{3,}[.]{1}[a-z]{2,3}$/), "email");
+
+		let panier = {
+			contact: contact, 
+			products: generate_products_id(products)
+		}
+
+		const options = {
+			method: 'POST',
+			body: JSON.stringify(panier),
+			headers: {
+				'Content-Type': 'application/json'
+			}	
+		}
+
+		if(checkedElements === 5){
+			fetch('http://localhost:3000/api/teddies/order', options)
+			.then(res => res.json())
+			.then(res => {
+				window.location.replace("file:///C:/Users/Arujan/Desktop/JWDP5-master/confirmation.html?total=" + total + "&order=" + res.orderId);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+		}
+	});
 }
